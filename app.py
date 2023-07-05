fb7df32c9861e8b53373918553397c82cb6ea0f6
Marco <104921935+marcolanfranchi@users.noreply.github.com>
2023-07-05 00:39:01 -0700

    added multiple selection for nbhds and crime types added marker icons for each crime type Next: - method to generate html for marker cards (address, date & time, crime)

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ from data_tools import CrimeDataHandler
 from map_plot import plot_on_map
 from background import set_bg_hack
 
+# ======================================================
 st.set_page_config(layout="centered")
 set_bg_hack("images/vanmap-nobg.png")
 
+# ======================================================
 vancouver_crime_df = pd.read_csv("data/crimedata_csv_all_years.csv")
 crimeData = CrimeDataHandler(vancouver_crime_df)
 
@@ -19,26 +21,27 @@ van_nbhds = crimeData.get_unique_sorted_vals('NEIGHBOURHOOD')
 crime_types = crimeData.get_unique_sorted_vals('TYPE')
 years = crimeData.get_unique_sorted_vals("YEAR")
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+# ======================================================
 
 st.title("Vancouver Crimes Map")
 st.markdown("Displays a map of crime locations in Vancouver from 2003 up to 2021 by time, neighbourhood, and type of crime.")
 
+# ======================================================
 selection_container = st.container()
 with selection_container:
     st.markdown("---")
     time_col, neighbourhood_col, crime_col = st.columns(3)
 
+    # ======================================================
     with time_col:
         year_type = st.radio(
             "Year:",
             options=["All (2003-2021)", "Range", "Custom"],
             index=2
         )
-        isRangeYears = False
         if year_type == "All (2003-2021)":
             year_selection = years
         if year_type == "Range":
-            isRangeYears = True
             from_year = st.number_input(
                 label="From Year:",
                 min_value=years[0],
@@ -56,40 +59,53 @@ with selection_container:
                 options= years,
                 default= 2021
             )
-
+    # ======================================================
     with neighbourhood_col:
-        nbhd_choice = st.selectbox(
+        nbhd_choice = st.multiselect(
             "Select one or more neighbourhoods",
-            # options= ['All'] + van_nbhds,
-            # default=['All']
-            options = van_nbhds,
-            index=0
+            options= ['All'] + van_nbhds,
+            default='Arbutus Ridge'
             )
-        # if nbhd_choice.count('All') > 0:
-        #     nbhds = van_nbhds
-        # else:
-        #     nbhds = nbhd_choice
-        nbhds = [nbhd_choice]
+        if nbhd_choice.count('All') > 0:
+            nbhds_selection = van_nbhds
+        else:
+            nbhds_selection = nbhd_choice
 
+    # ======================================================
     with crime_col:
-        crime_choice = st.selectbox(
-            "Select one or more crime types",
-            options= crime_types,
-            index=0
+        crime_choice = st.multiselect(
+            "Select one or more offence types",
+            options= ['All'] + crime_types,
+            default='Break and Enter Commercial'
             )
-        # if crime_choice.count('All') > 0:
-        #     crimes = crime_types
-        # else:
-        #     crimes = crime_choice
-        crimes = [crime_choice]
+        if crime_choice.count('All') > 0:
+            crimes_selection = crime_types
+        else:
+            crimes_selection = crime_choice
 
     st.markdown("---")
 
-map_data = crimeData.get_data(year=year_selection, nbhd=nbhds, crime_type=crimes)
+# ======================================================
+map_data = crimeData.get_data(year=year_selection, nbhd=nbhds_selection, crime_type=crimes_selection)
 # st.dataframe(map_data)
-st.text('Year(s): '+ str(year_selection))
-st.text('Neighbourhood: ' +  ','.join(nbhds))
-st.text('Offense Type: ' + ','.join(crimes))
+st.text('Year(s) ---------------------------------------------------------------------------')
+if year_type == 'Range':
+    st.text(str(year_selection[0]) + " to " + str(year_selection[-1]))
+elif year_type == 'All (2003-2021)':
+    st.text('All (2003-2021)')
+else:
+    st.text(', '.join(str(y) for y in [year_selection]))
+
+st.text('Neighbourhood(s) ------------------------------------------------------------------')
+if 'All' in nbhds_selection or len([n for n in nbhds_selection if n != 'All']) == len(van_nbhds):
+    st.text('All')
+else:  
+    st.text(', '.join(nbhds_selection))
+
+st.text('Offences --------------------------------------------------------------------------')
+for n in [crime for crime in crimes_selection if crime != 'All']:
+    count = list(map_data['TYPE']).count(n)
+    st.text(n + ": " + str(count))
 
 map_container = st.container()
 with map_container:
@@ -97,6 +113,6 @@ with map_container:
     if map_data.empty == False:    
         plot_on_map(map_data)
     else:
-        st.warning("No " + str(crimes) + "'s occured in " + str(nbhds) + " during " + str(year_selection))
+        st.warning("No " + str(crimes_selection) + "'s occured in " + str(nbhds_selection) + " during " + str(year_selection))
     st.markdown("---")
 
--- map_plot.py
+++ map_plot.py
@@ -17,8 +17,35 @@ def plot_on_map(df):
         
         #Add each row to the map
         loc = utm.to_latlon(row['X'], row['Y'], 10, 'N')
-        folium.Marker(location=list(loc),
-                    popup = popup, c=row['TYPE'],
-                    ).add_to(m)
+
+        folium.Marker(
+            location=list(loc),
+            popup = popup, 
+            icon=get_icon(row['TYPE'])
+        ).add_to(m)
 
     st_data = folium_static(m, width=700, height=500)
+
+def get_icon(crime_type):
+    icon = ""
+    color = "red"
+    if crime_type == 'Break and Enter Commercial':
+        icon="fa-sharp fa-solid fa-building"
+    elif crime_type == 'Break and Enter Residential/Other':
+        icon="fa-sharp fa-solid fa-house"
+    elif crime_type == 'Mischief':
+        icon="fa-sharp fa-solid fa-bomb"
+    elif crime_type == 'Other Theft':
+        icon="fa-sharp fa-solid fa-cart-shopping"
+    elif crime_type == 'Theft from Vehicle':
+        icon="fa-sharp fa-solid fa-car"
+    elif crime_type == 'Theft of Bicycle':
+        icon="fa-sharp fa-solid fa-bicycle"
+    elif crime_type == 'Theft of Vehicle':
+        icon="fa-sharp fa-solid fa-route"
+    elif crime_type == 'Vehicle Collision or Pedestrian Struck (with Fatality)':
+        icon="fa-sharp fa-solid fa-hospital"
+    elif crime_type == 'Vehicle Collision or Pedestrian Struck (with Injury)':
+        icon="fa-sharp fa-solid fa-car-burst"
+
+    return folium.Icon(color= color, icon=icon)
